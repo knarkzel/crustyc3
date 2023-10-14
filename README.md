b171541e4e43145f2b4a4e2d592f9cb9abaaf00a
Knarkzel <knarkzel@gmail.com>
2023-10-14 15:03:53 +0200

    readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# oddc3
+
+Connect your `esp32c3` with a cable that has data transfer capability, then do following:
+
+```
+git clone https://github.com/knarkzel/oddc3
+cd oddc3
+nix develop
+cargo run
+```
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, rust, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        rust.overlays.default
+      ];
+      pkgs = import nixpkgs {inherit system overlays;};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo-espflash
+          (rust-bin.nightly.latest.default.override {
+            extensions = ["rust-src"];
+          })
+        ];
+      };
+    });
+}
